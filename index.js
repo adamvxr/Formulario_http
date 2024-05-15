6093a32b927fc291abcf0dba7ff8c0c80faf1911
Adam <seuemail@example.com>
2024-05-15 12:05:30 -0300

    Att

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const porta = 3000;
+const host ='0.0.0.0';
+
+const app = express();
+
+app.use(express.static('./publico'));
+app.listen(porta, host, () => {
+ console.log(`Servidor rodando na porta http://${host}:${porta}`);
+})
